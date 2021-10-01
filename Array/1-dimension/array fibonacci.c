4c55104c58bf2dfa5f8c2df2a79c49dbb0022359
GopiPhoebe <87902397+GopiPhoebe@users.noreply.github.com>
2021-10-01 19:35:20 +0530

    Update array fibonacci.c

--- Array/1-dimension/array fibonacci.c
+++ Array/1-dimension/array fibonacci.c
@@ -1,25 +1,17 @@
-
-#include <stdio.h>
-#include <stdlib.h>
-int main()
+#include<stdio.h>
+void main()
 {
-	int a[100],i,n,s;
-	printf("enter the size");
+	int a,b,n,s;
+	printf("enter the number \n");
 	scanf("%d",&n);
-	a[0]=0;
-	a[1]=1;
-		s=0;
-	for(i=2; i<n; i++)
+	a=-1;
+	b=1;
+	s=0;
+	while(s<=n)
 	{
-		a[i]=a[i-1]+a[i-2];
-		
-		}
-		for(i=0; i<n; i++){
-		
-		printf(" %d ",a[i]);
-		
+	s=a+b;
+	a=b;
+	b=s;
+	printf("  %d  ",s);
 	}
-	system("pause");
-	return 0;
-	
-	}
+}
